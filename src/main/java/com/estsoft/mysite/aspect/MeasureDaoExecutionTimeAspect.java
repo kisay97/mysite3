f39a197e168b36156c8af67313fdfb6df392c441
bit <bit@bit_pc>
2016-05-03 12:06:45 +0900

    20160502 MeasureDaoExecutionTimeAspect 구현2

--- src/main/java/com/estsoft/mysite/aspect/MeasureDaoExecutionTimeAspect.java
+++ src/main/java/com/estsoft/mysite/aspect/MeasureDaoExecutionTimeAspect.java
@@ -11,20 +11,22 @@ import org.springframework.util.StopWatch;
 public class MeasureDaoExecutionTimeAspect {
 	@Around("execution( * *..dao.*.*(..) ) || execution( * *..service.*.*(..) ) || execution( * *..controller.*.*(..) )")
 	public Object around(ProceedingJoinPoint pjp) throws Throwable{
-		//Before
-		String taskName = pjp.getTarget().getClass() + "." + pjp.getSignature().getName();
+		//Before(전처리)
+		String taskName = pjp.getTarget().getClass() + "." + pjp.getSignature().getName(); //실행한 메서드의 이름을 받아온다
 		
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 		
-		Object result = pjp.proceed();
+		Object result = pjp.proceed(); //pjp.proceed()를 해주면 실제 함수 실행됨
 		
-		//After
+		//After(후처리)
 		stopWatch.stop();
 		
 		System.out.println( taskName + " : "
 							+ stopWatch.getTotalTimeMillis() + "millis");
 		
-		return result;
+		return result; //실행한 결과를 따로 돌려줘야함
 	}
+	
+	
 }
